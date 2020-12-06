9a01c29b8c8bc699be25fe544c8eba9b552d8156
17784955661 <892867807@qq.com>
2020-12-06 21:45:03 +0800

    init

--- AR_Pokemon/ViewController.swift
+++ AR_Pokemon/ViewController.swift
@@ -22,18 +22,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        // 添加默认环境光
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
         let configuration = ARWorldTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main){
+            configuration.detectionImages = imageToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images Successfully Added!")
+        }
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,29 +53,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
+
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        // 检测检测到的anchor是否是ARImageAnchor 因为还有可能是个plane或者point
+        if let imageAnchor = anchor as? ARImageAnchor {
+            print(imageAnchor.referenceImage.name)
+            print("detected")
+            // 设置一个平面长宽与图片一致
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            // 添加一个透明度
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            // 绕x轴旋转90度
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+            
+            // 将pokemon的模型添加到场景
+            if imageAnchor.referenceImage.name == "eevee-card"{
+                if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn"){
+                    if let pokeNode = pokeScene.rootNode.childNodes.first{
+                        planeNode.addChildNode(pokeNode)
+                        pokeNode.eulerAngles.x = Float.pi / 2
+                    }
+                }
+            }
+            
+            if imageAnchor.referenceImage.name == "oddish-card" {
+                if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn"){
+                    if let pokeNode = pokeScene.rootNode.childNodes.first{
+                        planeNode.addChildNode(pokeNode)
+                        pokeNode.eulerAngles.x = Float.pi / 2
+                    }
+                }
+            }
+
+        }
+        
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
